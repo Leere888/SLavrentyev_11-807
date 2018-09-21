d15290a452c92186d7d91929030d044418a4bac0
Leere888 <43326344+Leere888@users.noreply.github.com>
2018-09-21 10:38:48 +0300

    Add files via upload

---
+++ calc2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+ class ClassWork_3 {
+    public static void main (String[] args) {
+        boolean ans = true;
+        Scanner sc = new Scanner(System.in);
+        double r = 0;
+        double n1;
+        double n2;
+        while (true) {
+            if (ans) {
+                n1 = sc.nextDouble();
+            } else {
+                n1 = r;
+            }
+            String op = sc.next();
+            n2 = sc.nextDouble();
+            if (op.equals("+")) {
+                r = n1 + n2;
+            }
+            if (op.equals("-")) {
+                r = n1 - n2;
+            }
+            if (op.equals("/")) {
+                r = n1 / n2;
+            }
+            if (op.equals("*")) {
+                r = n1 * n2;
+            }
+            System.out.println(r);
+            System.out.println("Сбросить результат? y/n");
+            String answer = sc.next();
+            if (answer.equals("y")) {
+                ans = true;
+            } else {
+                ans = false;
+            }
+        }
+    }
+}
